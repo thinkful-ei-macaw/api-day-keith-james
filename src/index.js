5b08f2904aca31524555910f77e825b05fa511cf
James Souksavanh Moua <Smoua1558@gmail.com>
2020-02-21 14:24:23 -0500

    completed exercise 9

--- src/index.js
+++ src/index.js
@@ -12,19 +12,7 @@ const main = function () {
     .then(res => res.json())
     .then((items) => {
       items.forEach((item) => store.addItem(item));
-      shoppingList.render();
-
-      api.createItem(newItemName)
-  .then(res => res.json())
-  .then((newItem) => {
-    store.addItem(newItem);
-    render();
-  });
-
-      const item = store.items[0];
-      console.log('current name: ' + item.name);
-      store.findAndUpdate(item.id, { name: 'barbaz' });
-      console.log('new name: ' + item.name);  
+      shoppingList.render(); 
     });
 
  
