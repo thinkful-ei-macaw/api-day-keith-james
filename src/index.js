1ec678f6efcf6513caec6f90bf66fed77fe9a744
James Souksavanh Moua <Smoua1558@gmail.com>
2020-02-21 11:28:59 -0500

    completed exercise 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import './index.css';
 
 import shoppingList from './shopping-list';
 
+import api from './api';
+
+
 const main = function () {
+
+  api.getItems()
+    .then(res => console.log(res));
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
