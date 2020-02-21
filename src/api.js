af6ad7df6ce74777ddff5aa0a187e928439973df
James Souksavanh Moua <Smoua1558@gmail.com>
2020-02-21 11:56:35 -0500

    Completed exercise 4

--- src/api.js
+++ src/api.js
@@ -1,11 +1,22 @@
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/james-moua';
 
-function getItems() {
-  return Promise.resolve('A successful response!');
-  fetch(`${BASE URL}./items`)
-  .then(items => items.json());
+const getItems = function () {
+  return fetch(`${BASE_URL}/items`);
+};
+
+const createItem = function (name) {
+  const newItem = JSON.stringify({ name });
+
+  return fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: newItem
+  });
 };
 
 export default {
-  getItems
+  getItems,
+  createItem
 };
--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import $ from 'jquery';
 import 'normalize.css';
 import './index.css';
 
+import api from './api';
 import shoppingList from './shopping-list';
 
-import api from './api';
-
-
 const main = function () {
-
-  api.getItems()
-  
-  .then(res => res.json())
-  .then(res => console.log(res));
-
+  api.createItem('pears')
+    .then(res => res.json())
+    .then((newItem) => {
+      return api.getItems();
+    })
+    .then(res => res.json())
+    .then((items) => {
+      console.log(items);
+    });
 
   shoppingList.bindEventListeners();
   shoppingList.render();
